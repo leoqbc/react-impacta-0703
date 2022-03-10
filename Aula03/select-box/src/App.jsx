963ead32b44e74de0ea263592129af90a7dcb8a5
Leonardo Tumadjian <tumadjian@gmail.com>
2022-03-09 23:09:13 -0300

    Adiciona aula 3 com conteudo em aula 2

---
+++ Aula03/select-box/src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class App extends Component {
+    render() {
+        return (
+            <select>
+                <option value="valor1">Valor1</option>
+            </select>
+        );
+    }
+}
